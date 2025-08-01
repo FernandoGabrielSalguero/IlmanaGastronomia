10aae3908fe31fd46569ece81abcd110e2107622
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2025-07-31 22:11:14 -0300

    main

--- views/papa/papa_dashboard.php
+++ views/papa/papa_dashboard.php
@@ -41,7 +41,7 @@ $telefono = $_SESSION['telefono'] ?? 'Sin teléfono';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>AMPD</title>
+    <title>IlMana Gastronomia</title>
 
     <!-- Íconos de Material Design -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
